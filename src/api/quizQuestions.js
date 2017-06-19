556f52df542cb26d65a9726c92cf38a01faba6f8
Cedric Laier <ced.laier@gmail.com>
2017-06-19 16:38:43 +0200

    init schiffe quiz

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,30 @@
+var quizQuestions = [
+  {
+      question: "Magst du Züge?",
+      answers: [
+          {
+              type: "Züge",
+              content: "Ja"
+          },
+          {
+              type: "Flugzeuge",
+              content: "Nein"
+          }
+      ]
+  },
+  {
+      question: "Magst du Flugzeuge?",
+      answers: [
+          {
+              type: "Züge",
+              content: "Nein"
+          },
+          {
+              type: "Flugzeuge",
+              content: "Ja"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
